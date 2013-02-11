04c623af0b9794598a879c9c0ae5d217ecf72944
Neha Gupta <guptane6@msu.edu>
2013-02-11 18:16:31 -0500

    first commmit

---
+++ chef.py
@@ -0,0 +1,22 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='kitchen', durable=True)
+print ' [*] Waiting for orders. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+    time.sleep(10)
+    print " [x] Done"
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='kitchen')
+
+channel.start_consuming()
+
