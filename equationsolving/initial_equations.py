a67b9288cd682dcd3a8a21f0c92b1c545f0eeb39
Neha Gupta <guptane6@msu.edu>
2013-02-20 19:05:26 -0500

    Put RabbitMQ in wrapper class Changed timestamp for MongoDB <--May need edit

--- equationsolving/initial_equations.py
+++ equationsolving/initial_equations.py
@@ -27,6 +27,11 @@ class connect_to_rabbit:
 		connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))	
 		channel = connection.channel()
 		channel.queue_declare(queue = self.namequeue, durable = True)
+		return connnection,channel
+	def add_message(self, initial_eq_str, channel):
+		channel.basic_publish(exchange = '', routing_key = self.namequeue, body = intial_eq_str, properties = pika.BasicProperties(delivery_mode = 2))
+	def disconnect(self, connection):
+		coneection.close()
 
 #Connect to database:
 connect_eqsolver_ng = pymongo.Connection('localhost, 27017')
@@ -35,8 +40,8 @@ equation_will_solve_ng = db.will_solve_ng
 
 #Connect to rabbitMQ channel
 #unsolved_equations = queue name
-MQ_connection = rabbit_mq_connect(unsolved_equations)
-MQ_connection.connect()
+MQ_connection = rabbit_mq_connect('unsolved_equations')
+connection, channel = MQ_connection.connect()
 
 #Defining the things to go into the order from user input:
 #start_time = strftime("%Y-%m-%d %H:%M:%S")
@@ -48,7 +53,8 @@ intial_eq_str = "{'first equation': '" + name_str + "', 'second equation': '" + 
 initial_eq_dict = ast.literal_eval(initial_eq_str)	#Converts string to dict
 
 #Sends the string to the queue, where it will be picked up by to_from_h3.py
-channel.basic_publish(exchange = '', routing_key = 'unsolved_equations', body = intial_eq_str, properties = pika.BasicProperties(delivery_mode = 2))
+#channel.basic_publish(exchange = '', routing_key = 'unsolved_equations', body = intial_eq_str, properties = pika.BasicProperties(delivery_mode = 2))
+MQ_connection.add_message(initail_eq_str, channel)
 equation_will_solve_ng.insert(initial_eq_dict)		#Adds dictionary order to DB
 equation_will_solve_ng.insert({t : <time_t>, i : <ordinal> })
 
@@ -56,7 +62,7 @@ equation_will_solve_ng.insert({t : <time_t>, i : <ordinal> })
 print "[x] Sent order %r" % (intial_eq_str,)
 
 #Closes connection to channel to queue and then quits program
-connection.close()
+MQ_connection.disconnect(connection)
 
 
 
